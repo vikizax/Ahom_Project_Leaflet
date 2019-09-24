8ae7194a5c4547895175ec03c6742b0b27c89df5
vikizax <44132243+vikizax@users.noreply.github.com>
2019-09-24 22:11:50 +0530

    Updated main.js
    -> Using openstreetmap tile
    -> added copyright attribution of openstreetmap
    
    ->fixed Sukapha initial location

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-// Creating map options
-var mapOptions = {
-    center: [24.007201, 97.887975],
-    zoom: 10
-}
+// initial map view
+const map = L.map('map').setView([24.007201, 97.887975], 13);
+// copy right 
+const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+// tile source
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// setting leaflet tile
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
+// const marker = L.marker([lat, lon]).addTo(mymap);
 
-// Creating a map object
-var map = L.map('map', mapOptions);
-// var marker = L.marker([24.126767, 98.417790]).addTo(map);
-var circle = L.circle([24.007201, 97.887975], {
+// circle for Sukapha first destination
+const initialLocation = L.circle([24.007201, 97.887975], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 800
 }).addTo(map);
-// Creating a Layer object
-var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-// Adding layer to the map
-map.addLayer(layer);
